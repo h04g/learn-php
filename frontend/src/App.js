9d75992561355bcf525c0640d315de27a5f10f6c
ho4g <tranhoangghuyy@gmail.com>
2023-07-12 10:11:55 +0700

    first FE commit

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+import Home from './Component/Home'
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+        
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Component/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h1>ABC</h1>
+                    </div>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default Home;
